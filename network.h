994cad57c09a7d49a867929d83cffe2bca0a798b
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2013-11-08 17:13:18 +0200

    add message class

--- network.h
+++ network.h
@@ -1,8 +1,27 @@
+#ifndef SQUIGGLY_NET_H
+#define SQUIGGLY_NET_H
+
+extern "C"{
+#include<sys/socket.h>
+#include <netinet/in.h>	/* needed for sockaddr_in */
+}
+
 struct s_recieved {
   int count;
   const char* data;
 };
 typedef struct s_recieved Recieved;
 
+
+class Message{
+  uint16_t cmd;
+  uint32_t arg;
+public:
+  Message(char*);
+  char* encode(void);
+};
+
+
 int open_listening_socket(void);
 Recieved* get_data(int);
+#endif
